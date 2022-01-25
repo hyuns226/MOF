e8e66e0d7c35e0367c4044967f34237425d73d69
이현서 <>
2022-01-26 01:15:51 +0900

    [Style] 학원 수업 생성 화면 UI 구성 수정/ outlet 연결

--- MOF/Sources/MyPage/AcademyUser/AddClassViewController.swift
+++ MOF/Sources/MyPage/AcademyUser/AddClassViewController.swift
@@ -8,6 +8,90 @@
 import UIKit
 class AddClassViewController : UIViewController{
     
+    lazy var buttonList = [kpopButton,coreoButton,hiphopButton,girlsHiphopButton,waakingButton,popinButton,rockingButton,crumpButton,voguingButton,houseButton]
+    
+    @IBOutlet weak var regularClassButton: UIButton!
+    @IBOutlet weak var ondayClassButton: UIButton!
+    @IBOutlet weak var classNameTextField: UITextField!
+    @IBOutlet weak var classExplainTextView: UITextView!
+    @IBOutlet weak var TuitionTextView: UITextField!
+    @IBOutlet weak var classMemberNumTextField: UITextField!
+    @IBOutlet weak var classStartTimeOneButton: UIButton!
+    @IBOutlet weak var classEndTimeOneButton: UIButton!
+    @IBOutlet weak var classStartTimeTwoButton: UIButton!
+    @IBOutlet weak var classEndTimeTwoButton: UIButton!
+    @IBOutlet weak var teacherCollectionView: UICollectionView!
+    @IBOutlet weak var classPictureImageView: UIImageView!
+    @IBOutlet weak var classPictureDeleteButton: UIButton!
+    @IBOutlet weak var classPictureAddButton: UIButton!
+    
+    @IBOutlet weak var kpopButton: UIButton!
+    @IBOutlet weak var coreoButton: UIButton!
+    @IBOutlet weak var hiphopButton: UIButton!
+    @IBOutlet weak var girlsHiphopButton: UIButton!
+    @IBOutlet weak var waakingButton: UIButton!
+    @IBOutlet weak var popinButton: UIButton!
+    @IBOutlet weak var rockingButton: UIButton!
+    @IBOutlet weak var crumpButton: UIButton!
+    @IBOutlet weak var voguingButton: UIButton!
+    @IBOutlet weak var houseButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK:- FUNCTION
+    func setButtonLayout(){
+        
+        regularClassButton.layer.cornerRadius = 25
+        regularClassButton.layer.borderWidth = 1
+        regularClassButton.layer.borderColor = #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+        
+        ondayClassButton.layer.cornerRadius = 25
+        ondayClassButton.layer.borderWidth = 1
+        ondayClassButton.layer.borderColor = #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+        
+        classStartTimeOneButton.layer.borderWidth = 1
+        classStartTimeOneButton.layer.borderColor = #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+        
+        classEndTimeOneButton.layer.borderWidth = 1
+        classEndTimeOneButton.layer.borderColor = #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+        
+        classStartTimeTwoButton.layer.borderWidth = 1
+        classStartTimeTwoButton.layer.borderColor = #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+        
+        classEndTimeTwoButton.layer.borderWidth = 1
+        classEndTimeTwoButton.layer.borderColor = #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+        
+        for i in buttonList{
+            i!.layer.cornerRadius = 20
+            i!.layer.borderWidth = 1
+            i!.layer.borderColor = #colorLiteral(red: 0.1333333333, green: 0.1333333333, blue: 0.1333333333, alpha: 1)
+                  
+            
+        }
+    
+       
+    
+        
+    }
+    
+    @IBAction func regularClassButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func onedayClassButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func startTimeOneAction(_ sender: Any) {
+        
+    }
+    @IBAction func endTimeTwoAction(_ sender: Any) {
+    }
+    @IBAction func EndTimeOneAction(_ sender: Any) {
+    }
+    @IBAction func startTimeTwoAction(_ sender: Any) {
+    }
     
     @IBAction func addTeacherButtonAction(_ sender: Any) {
         
@@ -17,6 +101,12 @@ class AddClassViewController : UIViewController{
         
         
         
+    }
+    
+    @IBAction func deleteClassPicture(_ sender: Any) {
+    }
+    
+    @IBAction func addClassPicture(_ sender: Any) {
     }
     
     
